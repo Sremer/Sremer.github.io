062a321d7bfd171aacf41cb0c4a09ecaf8c9bf55
Seth Remer <seth.e.remer@gmail.com>
2022-10-08 17:25:42 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `hi, ${myName}`;
+    }
+  }
+
+
+myButton.onclick = () => {
+  setUserName();
+};
+
